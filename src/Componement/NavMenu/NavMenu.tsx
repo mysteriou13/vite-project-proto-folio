38a32896cb8a4d23451c704dcb926427f7a597da
Massa anthony <elio007@hotmail.fr>
2025-08-24 14:40:48 +0200

    add filtre array navlink if connection

--- src/Componement/NavMenu/NavMenu.tsx
+++ src/Componement/NavMenu/NavMenu.tsx
@@ -5,52 +5,57 @@ import { navSelector } from '../../Store/Selector/SelectorNavMenu'
 import LinkNavMenu from '../LinkNavMenu/LinkNavMenu'
 import { useEffect, useState } from 'react'
 import "./NavMenu.css"
-export default function NavMenu() {
- type LinkNav= {
-  _id:string,
-  name:string,
-  adress:string,
-  typelink:string
- }
- const dispatch = useAppDispatch();
-const { isLoggedIn, role } = useAuth();
-const { items } = navSelector()
-let [itemtab,setitemtab] = useState<LinkNav[]>([])
-const [filtreitem,setfilteritem] = useState<LinkNav[]>([])
 
-  // 🔹 Charger les données d au montage du composant
+export default function NavMenu() {
+  type LinkNav= {
+    _id: string;
+    name: string;
+    address: string;
+    typelink: string;
+  }
+
+  const dispatch = useAppDispatch();
+  const { isLoggedIn, role } = useAuth();
+  const { items } = navSelector();
+  const [filtreitem, setfilteritem] = useState<LinkNav[]>([]);
+
+  // load data navlink
   useEffect(() => {
     dispatch(readNavMenu());
   }, [dispatch]);
 
-  useEffect(()=>{
+  //Filter navlinks by status and role
+  useEffect(() => {
+    if (!Array.isArray(items.data)) return;
 
-  if (Array.isArray(items.data)) {
-    const filtered = items.data.filter(
-      (link: { typelink: string }) => link.typelink === "default" || link.typelink === "invisibleuserconnect"
-    );
+    let filtered: LinkNav[] = [];
+
+    if (!isLoggedIn) {
+      filtered = items.data.filter(
+        (link: { typelink: string }) =>
+          link.typelink === "default" || link.typelink === "invisibleuserconnect"
+      );
+    } else if (role === "user") {
+      filtered = items.data.filter((link:{typelink:string}) => link.typelink === "default");
+    } else if (role === "admin") {
+      filtered = items.data.filter(
+        (link: { typelink: string }) =>
+          link.typelink === "default" || link.typelink === "visibleadminconnect"
+      );
+    }
+
     setfilteritem(filtered);
-  }
-
-},[items])
+  }, [items, isLoggedIn, role]);
 
   return (
     <div className="box_header">
-   
-         
       <nav>
-        <ul className='ul_box'>
-          <>
-           {Array.isArray(filtreitem) && filtreitem.map((link: string) => (
-            
-           <LinkNavMenu data={link}/>
-            
+        <ul className="ul_box">
+          {filtreitem.map((link: LinkNav) => (
+            <LinkNavMenu data={link} key={link._id} />
           ))}
-          
-          </>
-          
         </ul>
       </nav>
     </div>
-  )
+  );
 }
