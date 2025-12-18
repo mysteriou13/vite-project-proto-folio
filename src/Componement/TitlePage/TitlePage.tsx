8f751641ca3c2e6da0d801841b4204d66ff66d06
Massa anthony <elio007@hotmail.fr>
2025-12-18 05:51:34 +0100

    correction display linknavmenu in navigation if user connnect or non connect

--- src/Componement/TitlePage/TitlePage.tsx
+++ src/Componement/TitlePage/TitlePage.tsx
@@ -1,6 +1,6 @@
 
 import './TitlePage.css';
-export default function TitlePage({ title, classStyle }) {
+export default function TitlePage({ title, classStyle }: { title: string; classStyle: string }) {
   return (
     
     <div className={classStyle}>{title}</div>
--- src/hooks/NavMenuHook.tsx
+++ src/hooks/NavMenuHook.tsx
@@ -1,4 +1,3 @@
-
 import { useAuth } from "../Store/Selector/SelectorUser";
 import { useApiData } from "./CallApi";
 import { useDispatch } from "react-redux";
@@ -6,44 +5,40 @@ import { setNav } from "../Store/Slice/NavSlice";
 import { LinkNav } from "../Interface/InterfaceNavmenu";
 
 export default function NavMenuHook() {
+  let datatoken = localStorage.getItem("token") || null;
+  let datarole = localStorage.getItem("role");
+  console.log("datarole", datarole);
+  const { handleRequest } = useApiData();
+  const dispatch = useDispatch();
 
-const {login,role} = useAuth();
-
-let datatoken = localStorage.getItem("token")||"";
-let datarole = localStorage.getItem("role")
-console.log("datarole",datarole)
-const {handleRequest} = useApiData()
-const dispatch = useDispatch()
-
-/*display nav link in the header*/
-const readNavLink = async ()=>{
-
-let data =  await handleRequest("/navmenu/readnavmenu","GET")
+  /*display nav link in the header*/
+  const readNavLink = async () => {
+    let data = await handleRequest("/navmenu/readnavmenu", "GET");
 
     let filtered: LinkNav[] = [];
 
-    if (!data) return; 
+   console.log('data toke, role', datatoken);
 
-    if (datatoken == "") {
+    if (!data) return;
+
+    if (!datatoken) {
       filtered = data.data.filter(
-        (link: { typelink: string; }) =>
+        (link: { typelink: string }) =>
           link.typelink === "default" ||
           link.typelink === "invisibleuserconnect"
       );
     } else if (datarole === "user") {
-      filtered = data.data.filter((link: { typelink: string; }) => link.typelink === "default");
+      filtered = data.data.filter(
+        (link: { typelink: string }) => link.typelink === "default"
+      );
     } else if (datarole === "admin") {
       filtered = data.data.filter(
-        (link: { typelink: string; }) =>
-          link.typelink === "default" ||
-          link.typelink === "visibleadminconnect"
+        (link: { typelink: string }) =>
+          link.typelink === "default" || link.typelink === "visibleadminconnect"
       );
-    }  
- dispatch(setNav({ data: filtered }));
+    }
+    dispatch(setNav({ data: filtered }));
+  };
 
-}
-
- return  {readNavLink}
-
-
+  return { readNavLink };
 }
